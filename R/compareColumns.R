7db2f55ee4d3b93a620e2474ac61fcf6aec6c0c1
lbraglia <lbraglia@gmail.com>
2014-10-20 10:52:23 +0200

    compareColumns with Reduce

--- R/compareColumns.R
+++ R/compareColumns.R
@@ -17,27 +17,19 @@ compareColumns <- function(db, operator = "<", row.id = NULL) {
 
   ## data should be a data.frame with no characters
   stopifnot( (is.data.frame(db)) & (!any(sapply(db, is.character))) )
+  first <- db[, -ncol(db)]
+  second <- db[, -1]
 
-  ## row.id handling
-  row.names(db) <- if(is.null(row.id)) {NULL} else {row.id}
+  ## matrix results
+  res <- Reduce(operator, list(first, second ))
+  colnames(res) <- paste(names(first), names(second), sep = ".vs.")
+  if(!is.null(row.id))
+    rownames(res) <- row.id
 
-  ## Raw results
-  res <- data.frame(rep(NA,nrow(db)))
-  for (i in 1:(ncol(db)-1))
-    eval(parse(text=sprintf("res[,i] <- db[,i] %s db[,i+1]", operator)))
-  names(res) <- paste(names(db)[2:(ncol(db))],
-                      names(db)[1:(ncol(db)-1)],
-                      sep=".vs.")
-
-  ## A report
+  ## A report with all test that doesn't respect the rule, but selecting
+  ## only useful records for print (those with at least 1 query/flag)
   report <- apply(res, 1, function(x) names(x)[x %in% FALSE])
-  names(report) <- row.id
-  ## select useful record for print (those with at least 1 query/flag
   select <- unlist(lapply(report, function(x) length(x)>0))
 
   list("results" = res, "report" = report[select])
 } 
-
-
-
-
