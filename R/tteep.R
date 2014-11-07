90f49b9ea1b62134ccdb4673f53c44a0717d3001
lbraglia <lbraglia@gmail.com>
2014-11-07 20:06:13 +0100

    tteep misc stuff

--- R/tteep.R
+++ R/tteep.R
@@ -1,21 +1,8 @@
 #' Common time to event end-points calculator.
 #' 
-#' 
 #' This function calculates common oncology time to event end-points (Overall
 #' survival, Progression free survival, time to relapse).
 #' 
-#' 
-#' @usage
-#' tteep(prog =!is.na(prog.date),
-#' prog.date=NULL,
-#' death = !is.na(death.date),
-#' death.date=NULL,
-#' start.date=NULL,
-#' last.fup=NULL,
-#' OS=TRUE,
-#' PFS=TRUE,
-#' TTP=TRUE,
-#' visual.check=FALSE) 
 #' @param prog Progression Indicator
 #' @param prog.date Progression Date
 #' @param death Death indicator
@@ -41,94 +28,84 @@ tteep <- function(prog =!is.na(prog.date), # Progression Indicator
                   TTP=TRUE, # Calculate TTP?
                   visual.check=FALSE)
 {
-    ## CHECK
-    if(	is.null(start.date)|is.null(last.fup))
-        stop("\n\tNeeded following vars (at least): 
+  ## CHECK
+  if(is.null(start.date)|is.null(last.fup))
+    stop("\n\tNeeded following vars (at least): 
 				\t - start.date, 
 				\t - last.fup")
+  
+  ## controlla che tutte le variabili indicatrici sian
+  ## correttamente codificate
+  if(!all(c(prog,death) %in% c(0,1))) 
+    stop("Not all prog,death %in% 01")
+  
+  ## controlla che laddove ci sia una variabile 
+  ## indicatrice a 1 vi sia la data corrispondente
+  
+  ## results list (to be converted to dataframe)
+  res <- list()
+  
+  if (OS) { # Overall survival
+    dummy <- death
+    last.date <- ifelse(dummy,death.date, last.fup) 
+    last.date <- as.Date(last.date, origin="1970-01-01")
+    time <- as.numeric(last.date - start.date)
+    res$os <- data.frame("dummy"=as.numeric(dummy),
+                         "last.date"=last.date,
+                         "time"=time)
+    rm(dummy,last.date,time)
+  } 
+  
+  if (PFS) { # Progression Free Survival
+    dummy <- death | prog
+    min.prog.death <- apply(cbind(death.date,prog.date), 1,
+                            min, na.rm=T)
+    min.prog.death <- as.Date(min.prog.death, origin="1970-01-01")
+    last.date <- ifelse(dummy, min.prog.death, last.fup)
+    last.date <- as.Date(last.date, origin="1970-01-01")
+    time <- as.numeric(last.date - start.date)
+    res$pfs <- data.frame("dummy"=as.numeric(dummy),
+                          "last.date"=last.date,
+                          "time"=time)
+    rm(dummy,last.date,time)
     
-    ## controlla che tutte le variabili indicatrici sian
-    ## correttamente codificate
-    if(!all(c(prog,death) %in% c(0,1))) 
-        stop("Not all prog,death %in% 01")
-	
-    ## controlla che laddove ci sia una variabile 
-    ## indicatrice a 1 vi sia la data corrispondente
-	
-    ## results list (to be converted to dataframe)
-    res <- list()
-	
-    if (OS) { # Overall survival
-        dummy <- death
-        last.date <- ifelse(dummy,death.date, last.fup) 
-        last.date <- as.Date(last.date, origin="1970-01-01")
-        time <- as.numeric(last.date - start.date)
-        res$os <- data.frame("dummy"=as.numeric(dummy),
-                             "last.date"=last.date,
-                             "time"=time)
-        rm(dummy,last.date,time)
-    } 
-	
-    if (PFS) { # Progression Free Survival
-        dummy <- death | prog
-        min.prog.death <- apply(cbind(death.date,prog.date), 1,
-                                min, na.rm=T)
-        min.prog.death <- as.Date(min.prog.death, origin="1970-01-01")
-        last.date <- ifelse(dummy, min.prog.death, last.fup)
-        last.date <- as.Date(last.date, origin="1970-01-01")
-        time <- as.numeric(last.date - start.date)
-        res$pfs <- data.frame("dummy"=as.numeric(dummy),
-                              "last.date"=last.date,
-                              "time"=time)
-        rm(dummy,last.date,time)
-        
-    } 
+  } 
 
-    if (TTP) { # Time to progression
-        dummy <- prog
-        min.death.lfup <- apply(cbind(death.date,last.fup), 1,
-                                min, na.rm=T)
-        min.death.lfup <- as.Date(min.death.lfup, origin="1970-01-01")
-        last.date <- ifelse(dummy, prog.date, min.death.lfup)
-        last.date <- as.Date(last.date, origin="1970-01-01")
-        time <- as.numeric(last.date - start.date)
-        res$ttp <- data.frame("dummy"=as.numeric(dummy),
-                              "last.date"=last.date,
-                              "time"=time)
-        rm(dummy,last.date,time)
-        
-    } 
+  if (TTP) { # Time to progression
+    dummy <- prog
+    min.death.lfup <- apply(cbind(death.date,last.fup), 1,
+                            min, na.rm=T)
+    min.death.lfup <- as.Date(min.death.lfup, origin="1970-01-01")
+    last.date <- ifelse(dummy, prog.date, min.death.lfup)
+    last.date <- as.Date(last.date, origin="1970-01-01")
+    time <- as.numeric(last.date - start.date)
+    res$ttp <- data.frame("dummy"=as.numeric(dummy),
+                          "last.date"=last.date,
+                          "time"=time)
+    rm(dummy,last.date,time)
+    
+  } 
 	
 	
-    ## Visualizza i risultati a video:
-    ## TODO: attualmente fallisce se visual.check=T e uno delle 
-    ## componenti è null perchè non viene fornito alla chiamata
-    ## ideale sarebbe comporre il dataframe "check" con i soli elementi
-    ## necessari (derivanti dalla chiamata es se OS=T,PFS=F,TTP=F)
-    
-    if (visual.check) {
-        check <- cbind(
-            "start.date"=start.date,
-            "prog"=as.numeric(prog),
-            "prog.date"=prog.date,
-            "death"=as.numeric(death),
-            "death.date"=death.date, 
-            "last.fup"=last.fup,
-            as.data.frame(res))
-        check <- check[ order(check$death,check$prog),]
-        View(check,title="Time to Event End Points: visual check")
-    }
-    
-    as.data.frame(res)
-    
-    ## tteep: Time to Event End Points
-    ## tteep(	prog=hn$recidiva,
-    ## prog.date=hn$data_rec,
-    ## death=hn$os, 
-    ## death.date=hn$data_decesso, 
-    ## start.date=hn$data_diagnosi,
-    ## last.fup=hn$ultimo_fu ,
-    ## visual.check=T)
-    
+  ## Visualizza i risultati a video:
+  ## TODO: attualmente fallisce se visual.check=T e uno delle 
+  ## componenti è null perchè non viene fornito alla chiamata
+  ## ideale sarebbe comporre il dataframe "check" con i soli elementi
+  ## necessari (derivanti dalla chiamata es se OS=T,PFS=F,TTP=F)
+  
+  if (visual.check) {
+    check <- cbind(
+      "start.date"=start.date,
+      "prog"=as.numeric(prog),
+      "prog.date"=prog.date,
+      "death"=as.numeric(death),
+      "death.date"=death.date, 
+      "last.fup"=last.fup,
+      as.data.frame(res))
+    check <- check[ order(check$death,check$prog),]
+    view(check)
+  }
+  
+  as.data.frame(res)
 }
 
