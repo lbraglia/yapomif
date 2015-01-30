4da800fa8113aa1f7d712b6505cf26c359308903
lbraglia <lbraglia@gmail.com>
2015-01-30 11:54:42 +0100

    test update

--- tests/testthat/test-datetime.R
+++ tests/testthat/test-datetime.R
@@ -1,13 +1,12 @@
 context("Date and time functions")
 
-today <- Sys.Date()
-now <- Sys.Time()
-Today <- dateMDY(Months(today, string=FALSE),
-                 Days(today),
-                 Years(today))
-## Now <- 
-
 test_that("dateMDY, Months, Days, Years", {
+  today <- Sys.Date()
+  ## now <- Sys.time()
+  Today <- dateMDY(Months(today, string=FALSE),
+                   Days(today),
+                   Years(today))
+
   expect_that(today, is_identical_to(Today))
   ## expect_that(now, is_identical_to(Now))
 })
