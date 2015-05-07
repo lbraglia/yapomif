337157b0d90b1cb9afd6f751bb510462dd7abe88
lbraglia <lbraglia@gmail.com>
2015-05-07 17:24:29 +0200

    setup errors

--- R/setup_errors.R
+++ R/setup_errors.R
@@ -14,8 +14,11 @@
 #'                            ))
 #' setup_errors(db$y, db$pt)
 #' @references Marcel van Herk, Errors and Margins in Radiotherapy,
-#'                            Seminars in Radiation Oncology, Vol 14, No 1
-#'                            (January), 2004 
+#' Seminars in Radiation Oncology, Vol 14, No 1 (January), 2004
+#' @references Joep Stroom and Ben Heijmen, Geometrical uncertainties,
+#' radiotherapy planning margins, and the ICRU-62 report, Radiotherapy and
+#' Oncology 64 (2002) 75-83
+#' 
 #' @export
 setup_errors <- function(x, group){
 
@@ -29,7 +32,13 @@ setup_errors <- function(x, group){
   rval$Sigma <- sd(means)
   ## Random error variability (RMS)
   rval$sigma <- sqrt(mean(vars))
-
+  ## PTV - ICRU 62
+  rval$ptv_icru <- sqrt(rval$Sigma^2 + rval$sigma^2)
+  ## PTV - Stroom
+  rval$ptv_stroom <- rval$Sigma * 2 + rval$sigma * 0.7
+  ## PTV - Van Herk
+  rval$ptv_van_herk <- rval$Sigma * 2.5 + rval$sigma * 0.7
+  
   ## Return
   unlist(rval)
   
